faf109d5a4f12c7744070fa9eb498d4a1ec9f98b
Simon Andersen <simo414u@stud.kea.dk>
2021-09-01 15:37:45 +0200

    Showing products

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
     <b-container>
       <b-row class="justify-content-center">
         <AddProduct />
-        <ListProduct />
+        <!-- We can pass data (props) to a component by simply using the v-bind directive (':' for shorthand). Here we bind the productList array to the ListProduct component and call the props 'productlist' -->
+        <ListProduct :products="productList" />
       </b-row>
     </b-container>
   </div>
@@ -14,13 +15,34 @@
 import AppHeader from "./components/AppHeader.vue";
 import AddProduct from "./components/AddProduct.vue";
 import ListProduct from "./components/ListProduct.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     AppHeader,
-    AddProduct,
-    ListProduct,
+    AddProduct, // Called child components
+    ListProduct, // It is best to keep the state of the data in the parent component and pass them to children with props. The children should NOT mutate the props, but instead EMIT events that may change the props at the parent level (and then the changed props are sent to the child anew)
+  },
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  methods: {
+    async getProductList() {
+      try {
+        let result = await axios.get("http://localhost:3000/products/");
+        // console.log(result.data);
+        this.productList = result.data;
+        console.log(this.productList);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.getProductList();
   },
 };
 </script>
--- src/components/ListProduct.vue
+++ src/components/ListProduct.vue
@@ -1,15 +1,27 @@
 <template>
   <b-col md="5" class="m-3">
     <div>
-      <b-card header="Add Product">
-        <b-card-text></b-card-text>
+      <b-card header="List of products">
+        <b-card-group deck>
+          <!-- v-for works as .forEach and iterates through the elems in the array, creating a li elem for each, assigning a unique key identifier. 'success' and 'danger' are predefined colors from Bootstrap-->
+          <b-card :border-variant="product.inventoryStatus? 'success' : 'danger'" align="center" v-for="product in products" :key="product.id">
+            <b-card-text>
+              <strong>{{product.name}}</strong>
+            </b-card-text>
+            <b-card-text>Price {{product.price}}</b-card-text>
+            <b-card-text>From: {{product.brand}}</b-card-text>
+            <b-card-text>{{product.inventoryStatus? 'In stock':'Not in stock'}}</b-card-text>
+          </b-card>
+        </b-card-group>
       </b-card>
     </div>
   </b-col>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["products"], // Write the props RECIEVED/PASSED to THIS component here
+};
 </script>
 
 <style>
