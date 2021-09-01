527d456dd2b44bdb5a1aa3ed72acb1e7968e11ee
Simon Andersen <simo414u@stud.kea.dk>
2021-09-01 10:40:29 +0200

    Adding form validation

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -2,16 +2,48 @@
   <b-col md="4" class="m-3">
     <div>
       <b-card header="Add Product">
-        <b-card-text></b-card-text>
-        <b-button href="#" variant="primary">Add Product</b-button>
+        <b-card-body>
+          <b-form autocomplete="off">
+            <b-form-group label="Name">
+              <b-form-input v-model="form.name" id="name" name="name" trim></b-form-input>
+            </b-form-group>
+            <b-form-group label="Price">
+              <b-form-input v-model="form.price" id="price" name="price" trim></b-form-input>
+            </b-form-group>
+            <b-form-group label="Brand">
+              <b-form-input v-model="form.brand " id="brand" name="brand" trim></b-form-input>
+            </b-form-group>
+            <b-form-group label="Inventory ?">
+              <b-form-radio v-model="form.inventoryStatus" name="inventorystatus" value="true">In stock</b-form-radio>
+              <b-form-radio v-model="form.inventoryStatus" name="inventorystatus" value="false">Out of stock</b-form-radio>
+            </b-form-group>
+          </b-form>
+        </b-card-body>
+        <b-button block variant="primary" @click="addProduct">Add Product</b-button>
       </b-card>
     </div>
   </b-col>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        price: "",
+        brand: "",
+        inventoryStatus: "",
+      },
+    };
+  },
+  methods: {
+    addProduct() {
+      console.log(this.form);
+    },
+  },
+};
 </script>
 
 <style>
-</style>
+</style> 
